7212eb6be8e1d08a5c1dd3f722fb4b8fa46782d6
Alexandros Trifyllis <alexandros.trifyllis@trasysgroup.com>
2017-03-31 16:02:18 +0300

    mvoe sample service data to sampleData.ts

---
+++ src/app/store/sampleData.ts
@@ -0,0 +1,9 @@
+import { User } from './users';
+
+export const sampleUsers: User[] = [
+	{ id: 1, username: 'user1', email: 'mail@test.com' },
+	{ id: 2, username: 'user2', email: 'mail2@test.com' },
+	{ id: 3, username: 'user3', email: 'mail3@test.com' },
+	{ id: 4, username: 'user4', email: 'mail4@test.com' },
+	{ id: 5, username: 'user5', email: 'mail5@test.com' }
+];
--- src/app/user-list/user.service.ts
+++ src/app/user-list/user.service.ts
@@ -1,3 +1,4 @@
+import { sampleUsers } from './../store/sampleData';
 import { User } from './../store/users';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
@@ -8,13 +9,6 @@ import 'rxjs/add/observable/of';
 export class UserService {
 
 	getUsers(): Observable<User[]> {
-		const users: User[] = [
-			{ id: 1, username: 'user1', email: 'mail@test.com' },
-			{ id: 2, username: 'user2', email: 'mail2@test.com' },
-			{ id: 3, username: 'user3', email: 'mail3@test.com' },
-			{ id: 4, username: 'user4', email: 'mail4@test.com' },
-			{ id: 5, username: 'user5', email: 'mail5@test.com' }
-		];
-		return Observable.of(users);
+		return Observable.of(sampleUsers);
 	}
 }
