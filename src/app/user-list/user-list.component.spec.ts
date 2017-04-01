6fc33650f049297216a6bc56564f02c1a103ee83
alx <otinanism@gmail.com>
2017-04-02 01:11:00 +0300

    add component test

--- src/app/user-list/user-list.component.spec.ts
+++ src/app/user-list/user-list.component.spec.ts
@@ -5,6 +5,7 @@ import { FieldsetModule } from 'primeng/components/fieldset/fieldset';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { sampleUsers } from '../store/sampleData';
 
 describe('UserListComponent', () => {
 	let component: UserListComponent;
@@ -25,10 +26,20 @@ describe('UserListComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(UserListComponent);
 		component = fixture.componentInstance;
+		component.users = sampleUsers;
 		fixture.detectChanges();
 	});
 
 	it('should create component', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should have users displaying in a table', () => {
+		const compiled = fixture.debugElement.nativeElement;
+		const headerRow = compiled.querySelector('thead tr');
+		const userRows = compiled.querySelectorAll('tr.ui-widget-content');
+
+		expect(headerRow).toBeDefined();
+		expect(userRows.length).toBe(5);
 	});
 });
