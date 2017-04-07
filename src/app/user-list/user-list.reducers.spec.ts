a8f92058e27ac8a4c7c1370d94caf900f10dc712
Alexandros Trifyllis <alexandros.trifyllis@trasysgroup.com>
2017-04-07 18:31:02 +0300

    fix reducer tests to use users reducer

--- src/app/user-list/user-list.reducers.spec.ts
+++ src/app/user-list/user-list.reducers.spec.ts
@@ -1,25 +1,23 @@
-import { reducer } from '../store/reducer-config';
-import { initialState } from 'app/user-list/users.reducer';
+
+import { initialState, usersReducer } from 'app/user-list/users.reducer';
 import { LoadUsersSuccessAction } from './user-list.actions';
 import { sampleUsers } from '../store/sampleData';
 
 
-describe('BooksReducer', () => {
+describe('UserListReducer', () => {
 	describe('undefined action', () => {
 		it('should return the default state', () => {
 			const action = {} as any;
-			const result = reducer(undefined, action);
-			// TODO why does the result contain the router state too???
-			expect(result).toEqual(jasmine.objectContaining({ userState: initialState }));
+			const result = usersReducer(undefined, action);
+			expect(result).toEqual(initialState);
 		});
 	});
 
 	describe('Load Users', () => {
 		it('should update the state with the users', () => {
 			const action = new LoadUsersSuccessAction(sampleUsers);
-			const result = reducer(undefined, action);
-			// TODO why does the result contain the router state too???
-			expect(result).toEqual(jasmine.objectContaining({ userState: {users: sampleUsers }}));
+			const result = usersReducer(undefined, action);
+			expect(result).toEqual({users: sampleUsers });
 		});
 	});
 });
