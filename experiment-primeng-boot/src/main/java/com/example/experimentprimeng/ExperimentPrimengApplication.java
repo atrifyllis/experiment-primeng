6d2a43d68208bc4fb4e9ebb832574e320f737737
otinanism <otinanism@gmail.com>
2017-09-26 13:06:09 +0300

    add Zuul to proxy requests to backend

--- experiment-primeng-boot/src/main/java/com/example/experimentprimeng/ExperimentPrimengApplication.java
+++ experiment-primeng-boot/src/main/java/com/example/experimentprimeng/ExperimentPrimengApplication.java
@@ -2,8 +2,10 @@ package com.example.experimentprimeng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ExperimentPrimengApplication {
 
 	public static void main(String[] args) {
