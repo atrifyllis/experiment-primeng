b7071d5e70f2eb0e21f139686c843f9741fa7166
otinanism <otinanism@gmail.com>
2018-07-24 15:23:27 +0300

    remove unused ActionTypes enum

--- src/app/user-list/user-list.actions.ts
+++ src/app/user-list/user-list.actions.ts
@@ -1,27 +1,5 @@
 import {User} from './../store/users';
-import {type} from '../util';
 import {action, payload, union} from 'ts-action';
-
-/**
- * For each action type in an action group, make a simple
- * enum object for all of this group's action types.
- *
- * The 'type' utility function coerces strings into string
- * literal types and runs a simple check to guarantee all
- * action types in the application are unique.
- */
-export const ActionTypes = {
-	LOAD_USERS: type('[User] Load'),
-	LOAD_USERS_SUCCESS: type('[User] Load Success'),
-	DELETE_USER: type('[User] Delete'),
-	DELETE_USER_SUCCESS: type('[User] Delete Success'),
-	DELETE_USER_FAILED: type('[User] Delete Failed'),
-	OPEN_UPDATE_USER_DIALOG: type('[User] Open Update User Dialog'),
-	CLOSE_UPDATE_USER_DIALOG: type('[User] Close Update User Dialog'),
-	UPDATE_USER: type('[User] Update User'),
-	UPDATE_USER_SUCCESS: type('[User] Update User Success'),
-	UPDATE_USER_FAILED: type('[User] Update User Failed'),
-};
 
 
 export const LoadUsersAction = action('[User] Load', payload<any>());
